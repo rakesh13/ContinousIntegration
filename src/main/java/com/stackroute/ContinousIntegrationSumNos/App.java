8d607e78c48acc652481cbf4a7bf6eef238d117d
rakesh13 <mail2rakeshpandit@gmail.com>
2017-07-29 15:45:53 +0530

    First Commit

---
+++ src/main/java/com/stackroute/ContinousIntegrationSumNos/App.java
@@ -0,0 +1,17 @@
+package com.stackroute.ContinousIntegrationSumNos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(new App().sumNos(10,20));
+    }
+    public int sumNos(int num1,int num2)
+    {
+    	return num1+num2;
+    }
+}
